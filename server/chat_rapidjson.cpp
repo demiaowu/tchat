8410b9a1a1147c4ab3e5f33aba23787bf68942ec
demiaowu <demiaowu@163.com>
2017-03-31 15:07:45 +0800

    add rapidjson

--- server/chat_rapidjson.cpp
+++ server/chat_rapidjson.cpp
@@ -3,3 +3,22 @@
 //
 
 #include "chat_rapidjson.h"
+#include "chat_user.h"
+
+#include <rapidjson/document.h>
+#include <rapidjson/writer.h>
+#include <rapidjson/stringbuffer.h>
+
+namespace chat {
+    namespace server {
+
+        std::string& rooms_to_string(std::set<chat_room_ptr>& rooms) {
+            return;
+        }
+
+        std::set<chat_room_ptr>& string_to_rooms(const std::string& str) {
+            return;
+        }
+
+    } // server namespace
+} // chat namespace
--- server/chat_rapidjson.h
+++ server/chat_rapidjson.h
@@ -4,5 +4,19 @@
 
 #ifndef TCHAT_CHAT_RAPIDJSON_H
 #define TCHAT_CHAT_RAPIDJSON_H
+#include <string>
+
+#include "chat_room.h"
+
+
+namespace chat {
+    namespace server {
+
+        std::string& rooms_to_string(std::set<chat_room_ptr>& rooms);
+
+        std::set<chat_room_ptr>& string_to_rooms(const std::string& str);
+
+    } // server namespace
+} // chat namespace
 
 #endif //TCHAT_CHAT_RAPIDJSON_H
